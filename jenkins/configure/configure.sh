dd8534a7dbcdf61cd427de8ebda1a2e1021cfe30
Andrei Gaspar <andrei@planeeet.com>
2019-04-04 15:36:48 +0000

    - Changed configure script approach

--- jenkins/configure/configure.sh
+++ jenkins/configure/configure.sh
@@ -8,22 +8,16 @@ if [ $# -eq 4 ]
         echo "Number of arguments is valid."
     else
             echo "Invalid number of arguments provided!"
-            echo "Expected: 4"
+	    echo "Expected: 4 (JWT_SECRET, ADMIN_EMAIL_ADDRESS, SUPPORT_EMAIL_ADDRESS, SUPPORT_EMAIL_PW)"
             echo "Provided: $#"
             exit 1
 fi
 
 
-ADMIN_SECRET=$1
+JWT_SECRET=$1
 ADMIN_EMAIL_ADDRESS=$2
 SUPPORT_EMAIL_ADDRESS=$3
 SUPPORT_EMAIL_PW=$4
-
-
-echo "***** Generating Dockerfile *****"
-
-
-cp Dockerfile-template  Dockerfile
 
 
 echo "*****************************************"
@@ -31,10 +25,10 @@ echo "** Injecting environment configuration **"
 echo "*****************************************"
 
 
-sed -i "s,<ADMIN_SECRET>,${ADMIN_SECRET},g" Dockerfile
-sed -i "s,<ADMIN_EMAIL_ADDRESS>,${ADMIN_EMAIL_ADDRESS},g" Dockerfile
-sed -i "s,<SUPPORT_EMAIL_ADDRESS>,${SUPPORT_EMAIL_ADDRESS},g" Dockerfile
-sed -i "s,<SUPPORT_EMAIL_PW>,${SUPPORT_EMAIL_PW},g" Dockerfile
+sed -i "s,<JWT_SECRET>,${JWT_SECRET},g" docker-cloud.yml
+sed -i "s,<ADMIN_EMAIL_ADDRESS>,${ADMIN_EMAIL_ADDRESS},g" docker-cloud.yml
+sed -i "s,<SUPPORT_EMAIL_ADDRESS>,${SUPPORT_EMAIL_ADDRESS},g" docker-cloud.yml
+sed -i "s,<SUPPORT_EMAIL_PW>,${SUPPORT_EMAIL_PW},g" docker-cloud.yml
 
 
 echo "*****************************************"
