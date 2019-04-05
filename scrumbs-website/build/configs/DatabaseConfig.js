a51be5f7af7f769cb2151503ba87eee82a7ab43d
Justin Johansson <connect@indiescripter.com>
2019-04-05 13:50:40 +1030

    build: wip

--- scrumbs-website/build/configs/DatabaseConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DatabaseConfig {
-}
-DatabaseConfig.URI = `mongodb://${process.env.ENV_DOCKER === "true" ? "mongo:27017" : "localhost"}/scrumbs-website`;
-exports.default = DatabaseConfig;
-//# sourceMappingURL=DatabaseConfig.js.map
