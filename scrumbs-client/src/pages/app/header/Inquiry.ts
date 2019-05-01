836cc9e02ecffdb0ee65fdae011030632c3c7dcb
Andrei Gaspar <andrei@planeeet.com>
2019-05-02 01:43:52 +0300

    - Inquiry view component, prepared elements

--- scrumbs-client/src/pages/app/header/Inquiry.ts
+++ scrumbs-client/src/pages/app/header/Inquiry.ts
@@ -24,6 +24,26 @@ const template = require( "../../../templates/header-inquiry.html" );
 
 
 export class Inquiry extends ViewComponent {
+    private overlay: HTMLElement;
+
+    private submitContainer: HTMLElement;
+    private successContainer: HTMLElement;
+    private failureContainer: HTMLElement;
+
+    private closeBtn: HTMLElement;
+    private submitBtn: HTMLElement;
+
+    private select: HTMLElement;
+    private selectText: HTMLElement;
+    private selectArrow: HTMLElement;
+
+    private dropdown: HTMLElement;
+    private optionBug: HTMLElement;
+    private optionRequest: HTMLElement;
+
+    private titleInput: HTMLElement;
+    private descriptionInput: HTMLElement;
+
 
 
 
@@ -31,7 +51,28 @@ export class Inquiry extends ViewComponent {
     constructor(view: View, container: HTMLElement) {
         super( view, container, "Inquiry" );
 
-        this.container.innerHTML = template;
+        this.container.innerHTML    = template;
+
+        this.overlay                = document.getElementById( "inquiry-overlay" );
+
+        this.submitContainer        = document.getElementById( "inquiry-submit-container" );
+        this.successContainer       = document.getElementById( "inquiry-failed-container" );
+        this.failureContainer       = document.getElementById( "inquiry-success-container" );
+
+        this.closeBtn               = document.getElementById( "inquiry-modal-close-btn" );
+        this.submitBtn              = document.getElementById( "inquiry-submit-btn" );
+
+        this.select                 = document.getElementById( "inquiry-select-dropdown-head" );
+        this.selectText             = document.getElementById( "inquiry-selected" );
+        this.selectArrow            = document.getElementById( "inquiry-dropdown-btn" );
+
+        this.dropdown               = document.getElementById( "inquiry-select-dropdown-body" );
+        this.optionBug              = document.getElementById( "inquiry-option-bug" );
+        this.optionRequest          = document.getElementById( "inquiry-option-feature" );
+        
+        this.titleInput             = document.getElementById( "inquiry-modal-title-input" );
+        this.descriptionInput       = document.getElementById( "inquiry-modal-description-input" );
+
 
 
     }
