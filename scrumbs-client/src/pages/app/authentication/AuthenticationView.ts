9301c002c610f0108ebeebd6ca3d57fef92184d5
Ildiko Gaspar <ildiko@ildiesign.com>
2019-09-06 14:11:43 +0300

    Revert "- Fixed authentication scroll bug"
    This reverts commit 8d1fcac20550885579f2ee36d39b4f1741c10628.

--- scrumbs-client/src/pages/app/authentication/AuthenticationView.ts
+++ scrumbs-client/src/pages/app/authentication/AuthenticationView.ts
@@ -27,6 +27,7 @@ const authenticationViewTemplate = require( "../../../templates/authentication-v
 
 
 
+
 export class AuthenticationView extends View {
     private authenticationLogin: ViewComponent;
     private authenticationSignUp: ViewComponent;
